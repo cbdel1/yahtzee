d5d131b790cd8470f31eb0b7211071a832a84746
Chase <chase.delong@icloud.com>
2022-10-08 13:11:59 -0400

    ref

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as $ from 'jquery'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -39,4 +38,3 @@ $(function(){
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
